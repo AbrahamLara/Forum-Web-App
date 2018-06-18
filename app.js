76182fb8b4e9bba59680bd1742a6ddfc85b04c63
Abraham Lara Granados <abrahamlara19@gmail.com>
2018-06-18 13:00:32 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres://'+process.env.POSTGRES_USER+':'+process.env.POSTGRES_PASSWORD+'@localhost/authentication');
+var bodyParser = require('body-parser');
+var port = 8080;
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+app.listen(port);
+console.log(`Listening on port ${port}`);
