a5dd1924b42d67dbe014555d78af50805d4e6939
Abraham Lara Granados <abrahamlara19@gmail.com>
2018-06-22 13:26:58 -0400

    Heroku

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('postgres://'+process.env.POSTGRES_USER+':'+process.env.POSTGRES_PASSWORD+'@'+pro.env.POSTGRES_HOST+'/'+process.env.POSTGRES_DATABASE);
 var bodyParser = require('body-parser');
-var port = 8080;
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
